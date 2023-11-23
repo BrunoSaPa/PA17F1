f085590bc587130692fd681f0335d6fb9d44f23a
Bruno Sánchez <bruno.toreto@hotmail.com>
2023-11-22 23:07:38 -0600

    Added required fields

--- Razor-Pages/Northwind.Web/obj/Debug/net7.0/Northwind.Web.AssemblyInfo.cs
+++ Razor-Pages/Northwind.Web/obj/Debug/net7.0/Northwind.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3a1306842af3405b6c5e6ade60f209aed0ba20d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Northwind.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
